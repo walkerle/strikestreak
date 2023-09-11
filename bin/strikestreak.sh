28e67e0f04185be634c2b57791067a7f871e1cdf
Walker Le <walker.le88@gmail.com>
2023-09-11 17:59:01 -0600

    fixed create, migrate, seed commands

--- bin/strikestreak.sh
+++ bin/strikestreak.sh
@@ -9,5 +9,4 @@ cp -a client/build/. public/
 
 # backend build commands
 bundle install
-bundle exec rake db:migrate db:seed
-# Will this work for postgresql?  May need `bundle exec rails db:create db:migrate db:seed` and `bundle exec rails db:drop?`
+bundle exec rails db:create db:migrate db:seed
