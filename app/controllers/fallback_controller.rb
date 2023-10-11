31126cf681452bcb2e1c8b13c2720bd149d2c910
Walker Le <walker.le88@gmail.com>
2023-10-10 18:41:29 -0600

    redirect on refresh, added more leaderboard data, worked on datetime

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,14 @@
 class FallbackController < ApplicationController
+
+  skip_before_action :authorized_user, only: :index
+
   def index
-    render file: 'public/index.html'
+    redirect_to "/"
+    # render file: 'public/index'
+    # puts '=============================================================================================='
+    # puts "params: #{params}"
+    # puts "params[:path]: #{params[:path]}"
+    # puts "@req: #{@req}"
+    # puts '=============================================================================================='
   end
 end
